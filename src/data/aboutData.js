31c4ec18f38920914443c63f34b9d4e3023d3f32
Gelespi <gilespikokya45@gmail.com>
2023-10-09 09:38:56 +0530

    about Change

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name's Gilespi. I'm a web designer and developer based in Mumbai.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description2: "I work on freelance projects and utilize the time to build my own products, all while actively seeking opportunities to learn and expand my skillset. This continuous learning process enhances my capabilities and fuels my entrepreneurial journey.",
     image: 2
 }
